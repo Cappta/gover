bcc13fabac61d56bf6dac8e66ad37e8f477c3231
rmterra <r.mirandaterra@gmail.com>
2017-08-21 15:10:33 -0300

    Fix some test spec typos

--- version_test.go
+++ version_test.go
@@ -8,23 +8,23 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	Convey("Given a base Version strucutre", t, func() {
+	Convey("Given a base Version structure", t, func() {
 		major := 1
 		minor := 2
 		build := 3
 		revision := 4
 		testBase := NewVersion(major, minor, build, revision)
 
-		Convey("Then Major should equal to provided Major", func() {
+		Convey("Then Major should be equal to provided Major", func() {
 			So(testBase.GetMajor(), ShouldEqual, major)
 		})
-		Convey("Then Minor should equal to provided Minor", func() {
+		Convey("Then Minor should be equal to provided Minor", func() {
 			So(testBase.GetMinor(), ShouldEqual, minor)
 		})
-		Convey("Then Build should equal to provided Build", func() {
+		Convey("Then Build should be equal to provided Build", func() {
 			So(testBase.GetBuild(), ShouldEqual, build)
 		})
-		Convey("Then Revision should equal to provided Revision", func() {
+		Convey("Then Revision should be equal to provided Revision", func() {
 			So(testBase.GetRevision(), ShouldEqual, revision)
 		})
 
@@ -39,16 +39,16 @@ func TestVersion(t *testing.T) {
 				Convey("Then version structure should not be nil", func() {
 					So(testBase, ShouldNotBeNil)
 				})
-				Convey("Then Major should equal to provided Major", func() {
+				Convey("Then Major should be equal to provided Major", func() {
 					So(testBase.GetMajor(), ShouldEqual, major)
 				})
-				Convey("Then Minor should equal to provided Minor", func() {
+				Convey("Then Minor should be equal to provided Minor", func() {
 					So(testBase.GetMinor(), ShouldEqual, minor)
 				})
-				Convey("Then Build should equal to provided Build", func() {
+				Convey("Then Build should be equal to provided Build", func() {
 					So(testBase.GetBuild(), ShouldEqual, build)
 				})
-				Convey("Then Revision should equal to provided Revision", func() {
+				Convey("Then Revision should be equal to provided Revision", func() {
 					So(testBase.GetRevision(), ShouldEqual, revision)
 				})
 			})
@@ -68,75 +68,75 @@ func TestVersion(t *testing.T) {
 			})
 		})
 
-		Convey("Given a Version strucutre with same values", func() {
+		Convey("Given a Version structure with same values", func() {
 			equalVersion := NewVersion(major, minor, build, revision)
-			Convey("Then base should equal", func() {
+			Convey("Then base should be equal", func() {
 				So(testBase, VersionShouldEqual, equalVersion)
 			})
 		})
-		Convey("Given a Version strucutre with a 5th zero value", func() {
+		Convey("Given a Version structure with a 5th zero value", func() {
 			fiveValuesVersion := NewVersion(major, minor, build, revision, 0)
-			Convey("Then base should equal", func() {
+			Convey("Then base should be equal", func() {
 				So(testBase, VersionShouldEqual, fiveValuesVersion)
 			})
 		})
 
-		Convey("Given a Version strucutre with a higher Major", func() {
+		Convey("Given a Version structure with a higher Major", func() {
 			higherMajor := NewVersion(major+1, minor, build, revision)
 			Convey("Then base should be lower", func() {
 				So(testBase, VersionShouldBeLowerThan, higherMajor)
 			})
 		})
-		Convey("Given a Version strucutre with a higher Minor", func() {
+		Convey("Given a Version structure with a higher Minor", func() {
 			higherMinor := NewVersion(major, minor+1, build, revision)
 			Convey("Then base should be lower", func() {
 				So(testBase, VersionShouldBeLowerThan, higherMinor)
 			})
 		})
-		Convey("Given a Version strucutre with a higher Build", func() {
+		Convey("Given a Version structure with a higher Build", func() {
 			higherBuild := NewVersion(major, minor, build+1, revision)
 			Convey("Then base should be lower", func() {
 				So(testBase, VersionShouldBeLowerThan, higherBuild)
 			})
 		})
-		Convey("Given a Version strucutre with a higher Revision", func() {
+		Convey("Given a Version structure with a higher Revision", func() {
 			higherRevision := NewVersion(major, minor, build, revision+1)
 			Convey("Then base should be lower", func() {
 				So(testBase, VersionShouldBeLowerThan, higherRevision)
 			})
 		})
-		Convey("Given a Version strucutre with a 5th positive version value", func() {
+		Convey("Given a Version structure with a 5th positive version value", func() {
 			fiveValuesVersion := NewVersion(major, minor, build, revision, 1)
 			Convey("Then base should be lower", func() {
 				So(testBase, VersionShouldBeLowerThan, fiveValuesVersion)
 			})
 		})
 
-		Convey("Given a Version strucutre with a lower Major", func() {
+		Convey("Given a Version structure with a lower Major", func() {
 			lowerMajor := NewVersion(major-1, minor, build, revision)
 			Convey("Then base should be greater", func() {
 				So(testBase, VersionShouldBeGreaterThan, lowerMajor)
 			})
 		})
-		Convey("Given a Version strucutre with a lower Minor", func() {
+		Convey("Given a Version structure with a lower Minor", func() {
 			lowerMinor := NewVersion(major, minor-1, build, revision)
 			Convey("Then base should be greater", func() {
 				So(testBase, VersionShouldBeGreaterThan, lowerMinor)
 			})
 		})
-		Convey("Given a Version strucutre with a lower Build", func() {
+		Convey("Given a Version structure with a lower Build", func() {
 			lowerBuild := NewVersion(major, minor, build-1, revision)
 			Convey("Then base should be greater", func() {
 				So(testBase, VersionShouldBeGreaterThan, lowerBuild)
 			})
 		})
-		Convey("Given a Version strucutre with a lower Revision", func() {
+		Convey("Given a Version structure with a lower Revision", func() {
 			lowerRevision := NewVersion(major, minor, build, revision-1)
 			Convey("Then base should be greater", func() {
 				So(testBase, VersionShouldBeGreaterThan, lowerRevision)
 			})
 		})
-		Convey("Given a Version strucutre with only 3 version values", func() {
+		Convey("Given a Version structure with only 3 version values", func() {
 			fifthValueVersion := NewVersion(major, minor, build)
 			Convey("Then base should be greater", func() {
 				So(testBase, VersionShouldBeGreaterThan, fifthValueVersion)
